782383902544f857856cd5d4140b161508d6c06a
Rian9124 <147829192+Rian9124@users.noreply.github.com>
2024-09-10 17:59:03 -0300

    tela de ações

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// inicio a função
 function start(){
-    let count = 0
-    while(count < 10){
-        console.log(count)
-        count ++
+    // crio a estrutura de repetição (enquanto)
+    while(true){
+        // crio uma variavel para sair do lupin
+        let opcao = "sair"
+        // crio as opções apartir do switch que me permite criar casos
+        switch(opcao){
+            // caso
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            // caso
+            case "listar":
+                console.log("Vamos listar")
+                break
+            // caso para sair com a variavel sair e o return para parar a estrutura de repetição
+            case "sair":
+                return
+        }
     }
 }
 start()
