657f55053c283c9194a6adc7c84167cb00bdf344
Rian9124 <147829192+Rian9124@users.noreply.github.com>
2024-09-12 15:02:26 -0300

    problemas marcados e desmarcados

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ async function listarMetas(){
         choices:[...metas],
         instructions:false,
     })
+    metas.forEach((m)=>{
+        m.checked=false
+    })
     if(respostas.length == 0){
         console.log("nenhuma meta selecionada")
         return
     }
-    metas.forEach((m)=>{
-        m.checked=false
-    })
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
             return m.value == resposta
